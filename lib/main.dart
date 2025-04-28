a737dc9d497257068e123f2944e70f0d0f096cf6
RihannaMattos <hannawzzy@gmail.com>
2025-04-28 08:21:52 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'tela2.dart'; // importa a nova tela
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App com Navegação',
+      home: const TelaInicial(),
+    );
+  }
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Tela2()),
+            );
+          },
+          child: Image.asset("assets/images/FIEB.PNG"),
+        ),
+      ),
+    );
+  }
+}
